4192becd7fd9ffaeed8fae2588ba63ce0f04954e
Andrew Quitadamo <aquitada@uncc.edu>
2015-11-05 11:06:45 -0500

    Added example submission script

---
+++ cluster/sub.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#######################################################################
+# ======= PBS OPTIONS ======= 
+### Specify queue to run - viper or Cobra
+#PBS -q viper
+### Set the job name
+#PBS -N job-namet
+### Specify the # of cpus for your job.
+#PBS -l nodes=2:ppn=4
+### Adjust walltime below (default walltime = 7 days, or 168 hours)
+### if you require > 7 days, INCREASE to estimated # hours needed
+### if you DON'T require 7 days DECREASE to estimated # hours needed
+### (hint: jobs with smaller walltime value tend to run sooner)
+#PBS -l walltime=168:00:00
+### pass the full environment
+#PBS -V
+# send PBS output to /dev/null  (we redirect it below)
+#PBS -o /dev/null
+#PBS -e /dev/null
+# ===== END PBS OPTIONS =====
+#######################################################################
+
+SHORT_JOBID=`echo $PBS_JOBID |cut -d. -f1`
+1>$PBS_O_WORKDIR/$PBS_JOBNAME-$SHORT_JOBID.out  2>$PBS_O_WORKDIR/$PBS_JOBNAME-$SHORT_JOBID.err
+cd $PBS_O_WORKDIR
+
+#Run code like on the command line. Ex:
+#bash script.sh
+#python script.py
+#perl script.pl
+#module load R
+#R --no-save < script.R
+#module load matlab
+#matlab -nodisplay <script.m
